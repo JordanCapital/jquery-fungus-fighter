a38a5aa5a5a1c4832dafcef5b17ac86d90ba462b
Jordan Capital <118566815+JordanCapital@users.noreply.github.com>
2023-02-24 13:45:39 +0300

    Update script.js
    declare varibles

--- script.js
+++ script.js
@@ -5,14 +5,34 @@ $(document).ready(onReady);
 // Example:
 // let fungusHP = 100;
 
+var fungusHP = 100;
+var attackPoints = 100;
+
 function onReady() {
-    
-    // Make sure you check the index.html file! 
+  $('.attack-btn').on('click', handleAttack);
+
+  // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
-    
-    
-    // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
-}
+
+  function handleAttack(event) {
+    let attackType = $(this).attr('class').split(' ')[1];
+
+    // Update state variables for attack and hit points with switch statement.
+    switch (attackType) {
+      case 'arcane-sceptre':
+        fungusHP -= 12;
+        attackPoints -= 14;
+        break;
+      case 'entangle':
+        fungusHP -= 23;
+        attackPoints -= 9;
+        break;
+      case 'dragon-blade':
+        fungusHP -= 38;
+        attackPoints -= 47;
+        break;
+      case 'star-fire':
+        fungusHP -= 33;
+        attackPoints -= 25;
+        break;
+    }
